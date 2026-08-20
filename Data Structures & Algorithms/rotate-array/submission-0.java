f5ae8aaf77e0edfef787df8f3854e7bc548f0c7b
HEMANTH-HS-HUGO <135624956+HEMANTH-HS-HUGO@users.noreply.github.com>
2026-08-20 23:58:01 +0530

    Add: rotate-array - submission-0

---
+++ Data Structures & Algorithms/rotate-array/submission-0.java
@@ -0,0 +1,33 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        int l = 0, r = nums.length-1;
+        while(l<r){
+            int temp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = temp;
+            l++;
+            r--;
+        }
+
+        l = 0;
+        r = k-1;
+        while(l<r){
+             int temp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = temp;
+            l++;
+            r--;
+        }
+
+        l = k;
+        r = nums.length-1;
+        while(l<r){
+             int temp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = temp;
+            l++;
+            r--;
+        }
+    }
+}
